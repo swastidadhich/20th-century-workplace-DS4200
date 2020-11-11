c8e9f94df30dad2996049db1a78505f7fe820891
Thura Naing <naing.t@northeastern.edu>
2020-11-10 22:04:04 -0500

    added title and labels

--- js/linechart.js
+++ js/linechart.js
@@ -9,8 +9,8 @@ function lineChart(data){
     var width  = 1500;
     var height = 500;
     var margin = {
-        top: 30,
-        bottom: 30,
+        top: 50,
+        bottom: 50,
         left: 50,
         right: 50
     };
@@ -53,6 +53,34 @@ function lineChart(data){
   .attr('d', line(data))
   .attr('class', 'dataLine');
 
+  // title
+  chartGroup.append("text")
+  .attr("x", (width / 2))             
+  .attr("y", 0 - (margin.top / 2))
+  .attr("text-anchor", "middle")  
+  .style("font-size", "16px") 
+  .style("text-decoration", "underline")  
+  .text("Number of Posts in October 2018 on UberPeople.net");
+
+  // text label for the x axis
+  // adjusted from D3 Axis Label Reference
+  chartGroup.append("text")             
+  .attr("transform",
+          "translate(" + (width/2) + " ," + 
+                           (height - margin.top - 10) + ")")
+  .style("text-anchor", "middle")
+  .text("Days")
+  .attr('class' , 'axisLabel');
+
+  // text label for the y axis
+  // adjusted from D3 Axis Label Reference
+  chartGroup.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 0 - margin.left + 20)
+  .attr("x",0 - (height / 2) + margin.top)
+  .style("text-anchor", "middle")
+  .text("Count")
+  .attr('class' , 'axisLabel');  
 }
 
 d3.csv('data/data.csv', function(d) {
