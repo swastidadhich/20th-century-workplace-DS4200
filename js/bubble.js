33948e735c14ec4e43ee0ecda8eb7d1142b15375
Thura Naing <naing.t@northeastern.edu>
2020-11-17 13:34:33 -0500

    update to read json file

--- js/bubble.js
+++ js/bubble.js
@@ -1,57 +1,45 @@
-let width = 450
-let height = 450
+d3.json('data/keywords.json').then(data => {
+  
+  let width = 900
+  let height = 900
 
-let data = d3.json('data/keywords.json')
-
-console.log(data)
-
-let svg = d3.select("#vis2")
-  	.append("svg")
+  let svg = d3.select("#vis2")
+    .append("svg")
     .attr("width", width)
     .attr("height", height)
     .style('background', '#efefef');
 
-var test = [{ "name": "A" , "r" : 50 },
- { "name": "B" , "r" : 40 },
-  { "name": "C" , "r" : 55 }, 
-{ "name": "D"  , "r" : 65},
- { "name": "E"  , "r" : 25},
-  { "name": "F"  , "r" : 25}, 
-{ "name": "G"  , "r" : 55},
- { "name": "H"  , "r" : 35}]
-
-let node = svg.append("g")
+  let node = svg.append("g")
   .selectAll("circle")
-  .data(test)
+  .data(data)
   .enter()
   .append("circle")
-  	.attr('id', d => d.name)
-    .attr("r", d => d.r)
+    .attr('id', d => d.keywords)
+    .attr("r", d => d.count / 60)
     .attr("cx", width / 2)
     .attr("cy", height / 2)
     .style("fill", "#69b3a2")
     .style("fill-opacity", 0.3)
     .attr("stroke", "#6B97EE")
 
-let text = svg.select("g")
-  .selectAll("text")
-  .data(test)
-  .enter()
-  .append("text")
-  	.attr("dx", width / 2)
-    .attr("dy", height / 2)
-  	.text(d => d.name)
+  let text = svg.select("g")
+    .selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+      .attr("dx", width / 2)
+      .attr("dy", height / 2)
+      .text(d => d.keywords)
 
-// forces features (referenced from Yan Holtz)
-let simulation = d3.forceSimulation()
+  let simulation = d3.forceSimulation()
     .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
     .force("charge", d3.forceManyBody().strength(0.5)) // Nodes are attracted one each other of value is > 0
-    .force("collide", d3.forceCollide().strength(.01).radius(70).iterations(1)) // Force that avoids circle overlapping
+    .force("collide", d3.forceCollide().strength(.01).radius(60).iterations(1)) // Force that avoids circle overlapping
 
 // Apply these forces to the nodes and update their positions.
 // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
 simulation
-    .nodes(test)
+    .nodes(data)
     .on("tick", function(d){
       node
           .attr("cx", function(d){ return d.x; })
@@ -59,4 +47,5 @@ simulation
       text
           .attr("dx", function(d){ return d.x; })
           .attr("dy", function(d){ return d.y; })
+          });
     });
