a3bac4d507d4f4dec9737cebe6e6cc77ceb8a008
s22d05 <71361332+s22d05@users.noreply.github.com>
2020-12-02 16:23:16 -0500

    update

--- js/linechart.js
+++ js/linechart.js
@@ -1,151 +1,260 @@
-let parseDate = d3.timeParse('%m/%d/%Y');
+/* global D3 */
+
 var slices = []
-var dispatch = d3.dispatch('selectionUpdated');
 
-function lineChart(data){
+// Initialize a line chart. Modeled after Mike Bostock's
+// Reusable Chart framework https://bost.ocks.org/mike/chart/
+function graphline() {
 
-    let maxDate  = d3.max(data, function(d){return d.date; });
-    let minDate  = d3.min(data, function(d){return d.date; });
-    let maxCount = d3.max(data, function(d){return d.count;});
+  console.log("hello");
 
-    let width  = 1500;
-    let height = 500;
-    let margin = {
-        top: 50,
-        bottom: 50,
-        left: 50,
-        right: 50
-    };
+  // Based on Mike Bostock's margin convention
+  // https://bl.ocks.org/mbostock/3019563
+  let margin = {
+      top: 50,
+      left: 50,
+      right: 50,
+      bottom: 50
+    },
+    width = 1500,
+    height = 1500,
+    xValue = d => d.date,
+    yValue = d => d.count,
+    xLabelText = '',
+    yLabelText = '',
+    yLabelOffsetPx = 0,
+    xScale = d3.scaleTime(),
+    yScale = d3.scaleLinear(),
+    ourBrush = null,
+    selectableElements = d3.select(null),
+    dispatcher;
+    selector = '#linechart';
 
-    // source: https://datawanderings.com/2019/10/28/tutorial-making-a-line-chart-in-d3-js-v-5/    
-    // d3.nest was retired, d3.group is the build-in function and was not working
-    // works like a dictionary, i.e. if Array for 'covid' exists, add it to existing
-    // else create a new wordObject
-    data.forEach((word) => { 
-        
+
+
+  // Create the chart by adding an svg to the div with the id
+  // specified by the selector using the given data
+  function chart(selector, data) {
+
+    data.forEach((word) => {
+
         let wordObject = slices.find(el => el.id === word.word );
 
-        if (!wordObject) { 
-          wordObject = { 
+        if (!wordObject) {
+          wordObject = {
             id: word.word,
             values: [{
-              date: +word.date,
-              count: +word.count
+              date: word.date,
+              count: word.count
             }]
           }
           slices.push(wordObject)
-          
-        } else { 
+
+        } else {
           slices[slices.findIndex(el => el.id === word.word)].values.push({
-            date: +word.date,
+            date: word.date,
             count: +word.count
           })
         }
       });
 
-    let svg = d3.select('#linechart')
-    .append('svg')
-    .attr('width' , width)
-    .attr('height', height)
-    .style('background', '#efefef');
+  console.log(slices);
+    let svg = d3.select(selector)
+      .append('svg')
+        .attr('preserveAspectRatio', 'xMidYMid meet')
+        .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+        .classed('svg-content', true);
 
-    let chartGroup = svg
-    .append('g')
-    .attr('transform','translate(' + margin.left +',' + margin.top + ')');
+    svg = svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    let xScale = d3.scaleTime()
-    .domain([minDate, maxDate])
-    .range([0, width - margin.left - margin.right]);
+    //Define scales
+    xScale
+      .domain([
+        d3.min(data, d => xValue(d)),
+        d3.max(data, d => xValue(d))
+      ])
+      .rangeRound([0, width]);
 
-    let yScale = d3.scaleLinear()
-    .domain([0, maxCount])
-    .range([height - margin.bottom - margin.top, 0]);
+    yScale
+      .domain([
+        d3.min(data, d => yValue(d)),
+        d3.max(data, d => yValue(d))
+      ])
+      .rangeRound([height, 0]);
 
-    let xAxis = d3.axisBottom(xScale);
-    chartGroup.append('g')
-      .attr('class', 'x axis')
-      .attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
-      .call(xAxis);
+    // X axis
+    let xAxis = svg.append('g')
+        .attr('transform', 'translate(0,' + (height) + ')')
+        .call(d3.axisBottom(xScale));
 
-    let yAxis = d3.axisLeft(yScale);
-      chartGroup.append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate(0, 0)')
-    .call(yAxis);
+    // Put X axis tick labels at an angle
+    xAxis.selectAll('text')
+        .style('text-anchor', 'end')
+        .attr('dx', '-.8em')
+        .attr('dy', '.15em')
+        .attr('transform', 'rotate(-65)');
+
+    // X axis label
+    xAxis.append('text')
+        .attr('class', 'axisLabel')
+        .attr('transform', 'translate(' + (width - 50) + ',-10)')
+        .text(xLabelText);
+
+    // Y axis and label
+    let yAxis = svg.append('g')
+        .call(d3.axisLeft(yScale))
+        .append('text')
+        .attr('class', 'axisLabel')
+        .attr('transform', 'translate(' + yLabelOffsetPx + ', -12)')
+        .text(yLabelText);
 
     let line = d3.line()
-    .x(function(d){return xScale(d.date);})    
-    .y(function(d){return yScale(d.count);})
+
+    .x(function(d) {
+      return xScale(d.date);
+    })
+
+    .y(function(d) {
+      return yScale(d.count);
+    })
 
     let lines = svg.selectAll("lines")
       .data(slices)
       .enter()
       // creates <g> block for each line
       .append("g")
-        .attr('transform', 'translate(50,50)')
 
-    lines.append("path")
+
+
+
+    // Add the line
+    lines.append('path')
     .attr("id", function(d) { return d.id }) // the id is the keyword
     .attr('class', 'line')
     .attr("d", function(d) { return line(d.values); })
     .style("stroke", "#000000")
 
-    // title
-    chartGroup.append("text")
-    .attr("x", (width / 2))             
-    .attr("y", 0 - (margin.top / 2))
-    .attr("text-anchor", "middle")  
-    .style("font-size", "16px") 
-    .style("text-decoration", "underline")  
-    .text("Keyword Mentions in Posts on UberPeople.net");
+    svg.call(brush);
 
-    // text label for the x axis
-    // adjusted from D3 Axis Label Reference
-    chartGroup.append("text")             
-    .attr("transform",
-            "translate(" + (width/2) + " ," + 
-                             (height - margin.top - 10) + ")")
-    .style("text-anchor", "middle")
-    .text("Days")
-    .attr('class' , 'axisLabel');
+    // Highlight points when brushed
+    function brush(g) {
+      const brush = d3.brush()
+        .on('start brush', highlight)
+        .on('end', brushEnd)
+        .extent([
+          [-margin.left, -margin.bottom],
+          [width + margin.right, height + margin.top]
+        ]);
 
-    // text label for the y axis
-    // adjusted from D3 Axis Label Reference
-    chartGroup.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 20)
-    .attr("x",0 - (height / 2) + margin.top)
-    .style("text-anchor", "middle")
-    .text("Count")
-    .attr('class' , 'axisLabel');  
-  }
+      ourBrush = brush;
 
-d3.csv('data/data.csv', function(d) {
-    return {
-      date: parseDate(d.date),
-      word : d.word,
-      count: +d.count
-    };
-}).then(lineChart);
+      g.call(brush); // Adds the brush to this element
 
-dispatch.on("selectionUpdated", function(selectedData) {
-  console.log("heard")
-  if (!arguments.length) return;
+      // Highlight the selected circles.
+      function highlight(event, d) {
+        if (event.selection === null) return;
+        const [
+          [x0, y0],
+          [x1, y1]
+        ] = event.selection;
+        points.classed('selected', d =>
+          x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
+        );
 
-      // array for selected lines from dispatch call
-      let selectedLines = [];
-      for (let i = 0; i < selectedData.length; i++) { // first go through and construct an array with all the selected word strings
-        let word = selectedData[i].keywords || selectedData[i].data.keywords
-        selectedLines.push(word);
+        // Get the name of our dispatcher's event
+        let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
+
+        // Let other charts know
+        dispatcher.call(dispatchString, this, svg.selectAll('.selected').data());
       }
 
-      // get all lines matching id with keywords
-      let lines = document.getElementsByClassName('line');
-      for (let i = 0; i < lines.length; i++) { // go through all the lines and show/hide depending on if the word is selected or not
-        if (selectedLines.includes(lines[i].id)) {
-          lines[i].style.display = 'block'
-        } else {
-          lines[i].style.display = 'none'
+      function brushEnd(event, d) {
+        // We don't want infinite recursion
+        if(event.sourceEvent !== undefined && event.sourceEvent.type!='end'){
+          d3.select(this).call(brush.move, null);
         }
       }
-    });
+    }
+
+    return chart;
+  }
+
+  // The x-accessor from the datum
+  function X(d) {
+    return xScale(xValue(d));
+  }
+
+  // The y-accessor from the datum
+  function Y(d) {
+    return yScale(yValue(d));
+  }
+
+  chart.margin = function (_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function (_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function (_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  chart.x = function (_) {
+    if (!arguments.length) return xValue;
+    xValue = _;
+    return chart;
+  };
+
+  chart.y = function (_) {
+    if (!arguments.length) return yValue;
+    yValue = _;
+    return chart;
+  };
+
+  chart.xLabel = function (_) {
+    if (!arguments.length) return xLabelText;
+    xLabelText = _;
+    return chart;
+  };
+
+  chart.yLabel = function (_) {
+    if (!arguments.length) return yLabelText;
+    yLabelText = _;
+    return chart;
+  };
+
+  chart.yLabelOffset = function (_) {
+    if (!arguments.length) return yLabelOffsetPx;
+    yLabelOffsetPx = _;
+    return chart;
+  };
+
+  // Gets or sets the dispatcher we use for selection events
+  chart.selectionDispatcher = function (_) {
+    if (!arguments.length) return dispatcher;
+    dispatcher = _;
+    return chart;
+  };
+
+  // Given selected data from another visualization
+  // select the relevant elements here (linking)
+  chart.updateSelection = function (selectedData) {
+    if (!arguments.length) return;
+
+    // Select an element if its datum was selected
+    selectableElements.classed('selected', d =>
+      selectedData.includes(d)
+    );
+  };
+
+  return chart;
+}
